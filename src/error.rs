42592b991a95f62fc3829817168ce1ebbe7869ae
jamiegyoung <jamiegyoung22@gmail.com>
2021-07-17 19:23:47 +0100

    Refactored error Added documentation and removed redundant errors

--- src/error.rs
+++ src/error.rs
@@ -2,23 +2,26 @@ use json_dotpath::Error as JsonDotPathError;
 use serde_json::Error as SerdeJsonError;
 use std::{error, fmt, io};
 
+/// The `Error` type is an enum for all errors that can be thrown by this library.
 #[derive(Debug)]
 pub enum Error {
+    /// `Io` errors are errors that occur when reading from or writing to a file.
     Io(io::Error),
-    Write,
-    Read,
+    /// `NotFound` errors are errors that occur when a requested path is not found.
     NotFound,
+    /// `DotPath` errors are errors that occur when using the `JsonDotPath` library.
     DotPath(JsonDotPathError),
+    /// `SerdeJson` errors are errors that occur when using the `SerdeJson` library.
     Serde(SerdeJsonError),
+    /// `ConfigDir` errors are errors that occur when locating the config directory. 
     ConfigDir,
 }
+
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::Io(ref err) => err.fmt(f),
-            Error::Write => write!(f, "Store write error"),
-            Error::Read => write!(f, "Store read error"),
             Error::NotFound => write!(f, "Store not found"),
             Error::DotPath(err) => err.fmt(f),
             Error::Serde(err) => err.fmt(f),
@@ -31,8 +34,6 @@ impl error::Error for Error {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             Error::Io(ref err) => err.source(),
-            Error::Write => None,
-            Error::Read => None,
             Error::NotFound => None,
             Error::DotPath(ref err) => err.source(),
             Error::Serde(ref err) => err.source(),
@@ -41,18 +42,21 @@ impl error::Error for Error {
     }
 }
 
+/// A function to convert io::Error to Error.
 impl From<io::Error> for Error {
     fn from(e: io::Error) -> Error {
         Error::Io(e)
     }
 }
 
+/// A function to convert serde_json::Error to Error.
 impl From<SerdeJsonError> for Error {
     fn from(e: serde_json::Error) -> Error {
         Error::Serde(e)
     }
 }
 
+/// A function to convert json_dotpath::Error to Error.
 impl From<JsonDotPathError> for Error {
     fn from(e: JsonDotPathError) -> Error {
         Error::DotPath(e)
